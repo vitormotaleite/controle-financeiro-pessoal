b2807255ab03823d0cb34a63a59bfdfc5ffe8e9c
vitormotaleite <vitor.mota.diniz@ccc.ufcg.edu.br>
2025-05-31 15:43:03 -0300

    camada service e registro

--- src/main/java/com/example/financeiro/resources/CategoriaResource.java
+++ src/main/java/com/example/financeiro/resources/CategoriaResource.java
@@ -1,20 +1,34 @@
 package com.example.financeiro.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.financeiro.entities.Categoria;
+import com.example.financeiro.services.CategoriaService;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
+    @Autowired
+    private CategoriaService categoriaService;
+
     @GetMapping
-    public ResponseEntity<Categoria> findAll() {
-        Categoria c = new Categoria(1L, "Jogos");
-        return ResponseEntity.ok(c);
+    public ResponseEntity<List<Categoria>> findAll() {
+        List<Categoria> list = categoriaService.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Categoria> findById(@PathVariable Long id) {
+        Categoria obj = categoriaService.findCategoriaByid(id);
+        return ResponseEntity.ok().body(obj);
     }
     
 }
---
+++ src/main/java/com/example/financeiro/services/CategoriaService.java
@@ -0,0 +1,26 @@
+package com.example.financeiro.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.financeiro.entities.Categoria;
+import com.example.financeiro.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+    
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    public List<Categoria> findAll() {
+        return categoriaRepository.findAll();
+    }
+
+    public Categoria findCategoriaByid(Long id) {
+        Optional<Categoria> obj = categoriaRepository.findById(id);
+        return obj.get();
+    }
+}
